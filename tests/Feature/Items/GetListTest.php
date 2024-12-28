0f72abef8fb2286e42fdf0f3bf725f7f8625068f
dananjayarumesh <rumeshpriv@gmail.com>
2024-12-28 12:29:41 +0000

    get items list feature tests

--- tests/Feature/Items/GetListTest.php
+++ tests/Feature/Items/GetListTest.php
@@ -23,9 +23,59 @@ class GetListTest extends TestCase
 
         $response->assertSuccessful();
         $response->assertJson([
-            'items' => $items->toArray()
+            'page' => 1,
+            'per_page' => 10,
+            'data' => $items->toArray()
+        ]);
+    }
+
+    public function testGetListPagination(): void
+    {
+        $items = Item::factory()->count(10)->create();
+
+        // check page 1 results
+        $response = $this->get(
+            '/api/items?page=1&per_page=2',
+            $this->getAuthHeaders()
+        );
+        $response->assertSuccessful();
+        $response->assertJson([
+            'page' => 1,
+            'per_page' => 2,
+            'data' => array_slice($items->toArray(), 0, 2)
         ]);
 
-        $response->assertStatus(200);
+        // check page 2 results
+        $response = $this->get(
+            '/api/items?page=2&per_page=2',
+            $this->getAuthHeaders()
+        );
+        $response->assertSuccessful();
+        $response->assertJson([
+            'page' => 2,
+            'per_page' => 2,
+            'data' => array_slice($items->toArray(), 2, 2)
+        ]);
+    }
+
+    public function testGetListValidation(): void
+    {
+        $response = $this->get(
+            '/api/items?page=test&per_page=test',
+            $this->getAuthHeaders()
+        );
+        $response->assertStatus(422);
+        $response->assertJson([
+            'errors' => [
+                [
+                    'page' => [
+                        0 => 'The page field must be a number.'
+                    ],
+                    'per_page' => [
+                        0 => 'The per page field must be a number.'
+                    ],
+                ]
+            ]
+        ]);
     }
 }

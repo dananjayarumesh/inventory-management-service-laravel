3d98380eccd29e867785cbc0dbbca7a76e64669c
dananjayarumesh <rumeshpriv@gmail.com>
2025-01-04 17:45:23 +0000

    token generation logic update

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,23 +3,24 @@
 namespace App\Services;
 
 use App\Exceptions\AuthFailedException;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthService implements AuthServiceInterface
 {
     public function getAccessTokenByCred(string $email, string $password): string
     {
-        if (
-            !$accessToken = Auth::attempt(
-                compact(
-                    'email',
-                    'password'
-                )
-            )
-        ) {
+        $token = auth('api')->attempt(compact(
+            'email',
+            'password'
+        ));
+
+        if (!$token) {
             throw new AuthFailedException('Invaid credentials provided.');
         }
-        return $accessToken;
+
+        return $token;
     }
 }
